a71ad386f91edb739a0494fc5385552725c436f0
Blake Contreras <shardsofsilver@gmail.com>
2016-10-04 10:33:09 -0700

    Add forms module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component'
 import { PhraseComponent } from './phrase/phrase.component'
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ 
+    BrowserModule,
+    FormsModule 
+  ],
   declarations: [ 
     AppComponent,
-    PhraseComponent ],
+    PhraseComponent 
+  ],
   bootstrap: [ AppComponent ]
 })
 
--- dist/app.module.js
+++ dist/app.module.js
@@ -10,6 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
 var phrase_component_1 = require('./phrase/phrase.component');
 var AppModule = (function () {
@@ -17,10 +18,14 @@ var AppModule = (function () {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule
+            ],
             declarations: [
                 app_component_1.AppComponent,
-                phrase_component_1.PhraseComponent],
+                phrase_component_1.PhraseComponent
+            ],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
